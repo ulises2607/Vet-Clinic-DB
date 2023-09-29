b312cba03bad0fa532160e0de2a7363880dea290
Ulises Cappa <ulisescappa@gmail.com>
2023-09-29 12:27:04 -0300

    Update animals table and add README file

--- schema.sql
+++ schema.sql
@@ -2,4 +2,10 @@
 
 CREATE TABLE animals (
     name varchar(100)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INTEGER NOT NULL DEFAULT 0,
+    neutered BOOLEAN NOT NULL DEFAULT FALSE,
+    weight DECIMAL(6,2) NOT NULL DEFAULT 0.00
 );
