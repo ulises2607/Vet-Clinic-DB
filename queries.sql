dcc86343cfd188777fe674211cd139e72caf1165
Ulises Cappa <ulisescappa@gmail.com>
2023-09-29 23:41:25 -0300

    add trasactions queries. Update the animals table modifying the negative weight of animals

--- queries.sql
+++ queries.sql
@@ -33,3 +33,73 @@ WHERE name != 'Gabumon';
 SELECT *
 FROM animals
 WHERE weight >= 10.4 AND weight <= 17.3;
+
+
+
+-- Start a transaction
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+
+SELECT * FROM animals;
+
+-- Rollback the transaction to revert the changes
+ROLLBACK;
+
+-- Verify that the "species" column has reverted to its original state
+SELECT * FROM animals;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+SELECT * FROM animals;
+
+update animals
+set species = 'pokemon'
+where species is null;
+
+SELECT * FROM animals;
+
+-- Commit the transaction to make the changes permanent
+COMMIT;
+
+BEGIN;
+
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+-- Start a transaction
+BEGIN;
+
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+
+SAVEPOINT my_savepoint;
+
+
+UPDATE animals
+SET weight = weight * -1;
+
+
+ROLLBACK TO my_savepoint;
+
+
+UPDATE animals
+SET weight = weight * -1
+WHERE weight < 0;
+
+
+COMMIT;
+
+SELECT * FROM animals;
