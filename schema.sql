938894d5c23c4884a69e7b924d689a708fb4a3ec
Ulises Cappa <ulisescappa@gmail.com>
2023-09-30 22:23:14 -0300

    Create vets, visits and specializations tables with atributes and correspondent keys

--- schema.sql
+++ schema.sql
@@ -41,5 +41,30 @@ ADD CONSTRAINT fk_owner
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
 
+-- Next section
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    age INTEGER NOT NULL,
+    date_of_graduation DATE NOT NULL,
+);
+
+CREATE TABLE visits (
+    visit_id serial PRIMARY KEY,
+    animal_id integer,
+    vet_id integer,
+    visit_date date,
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
+
+CREATE TABLE specializations (
+    vet_id integer,
+    species_id integer,
+    PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id),
+    FOREIGN KEY (species_id) REFERENCES species (id)
+);
 
 
